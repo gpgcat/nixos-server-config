1a879b5c7d6839e24c104e080c59cf7df705b164
gpgcat <gpgcat@proton.me>
2026-04-29 00:18:21 +0300

    fix disko grub device duplication bug

--- hosts/chocola/default.nix
+++ hosts/chocola/default.nix
@@ -5,10 +5,7 @@
     ./hardware-configuration.nix
   ];
   
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-  };
+  boot.loader.grub.enable = true;
   
   networking = {
     useDHCP = false;
